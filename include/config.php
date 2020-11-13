849b9f392ccb255478b7f1db6f8e63cc47a3d34c
Amry Zulfa Al Husna <amryzoelfa@gmail.com>
2020-11-13 12:59:50 +0700

    v3

---
+++ include/config.php
@@ -0,0 +1,7 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['new-9288'] = array ('1'=>'new-9288!','new-9288@|@','new-9288#|#','new-9288%','new-9288^','new-9288&Rp','new-9288*10','new-9288(1','new-9288)','new-9288=10','new-9288@!@disable');
+$data['new-6396'] = array ('1'=>'new-6396!','new-6396@|@','new-6396#|#','new-6396%','new-6396^','new-6396&Rp','new-6396*10','new-6396(1','new-6396)','new-6396=10','new-6396@!@disable');
+$data['HEHA'] = array ('1'=>'HEHA!202.169.235.14','HEHA@|@sate','HEHA#|#q2GmaJynnKal','HEHA%CLEON','HEHA^hotspot.cleon.info','HEHA&Rp','HEHA*10','HEHA(1','HEHA)','HEHA=10','HEHA@!@disable');
