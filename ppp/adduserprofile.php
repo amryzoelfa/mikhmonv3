3b775851531cad475613c5e72cf29ee986c65a64
afiffarishudaifi <afiffaris5@gmail.com>
2020-11-13 16:47:42 +0700

    isi ppp

---
+++ ppp/adduserprofile.php
@@ -0,0 +1,128 @@
+<?php
+/*
+ *  Copyright (C) 2018 Laksamadi Guko.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+session_start();
+// hide all error
+error_reporting(0);
+if (!isset($_SESSION["mikhmon"])) {
+  header("Location:../admin.php?id=login");
+} else {
+
+  if (isset($_POST['name'])) {
+    $name = (preg_replace('/\s+/', '-', $_POST['name']));
+    $localaddress = ($_POST['localaddress']);
+    $remoteaddress = ($_POST['remoteaddress']);
+    $bridge = ($_POST['bridge']);
+    $ratelimit = ($_POST['ratelimit']);
+    $onlyone = ($_POST['onlyone']);
+
+    $API->comm("/ppp/profile/add", array(
+      /*"add-mac-cookie" => "yes",*/
+      "name" => "$name",
+      "local-address" => "$localaddress",
+      "remote-address" => "$remoteaddress",
+      "bridge" => "$bridge",
+      "rate-limit" => "$ratelimit",
+      "only-one" => "$onlyone",
+    ));
+  }
+}
+?>
+<div class="row">
+  <div class="col-8">
+    <div class="card box-bordered">
+      <div class="card-header">
+        <h3><i class="fa fa-plus"></i> Add PPP Profiles <small id="loader" style="display: none;"><i><i class='fa fa-circle-o-notch fa-spin'></i> Processing... </i></small></h3>
+      </div>
+      <div class="card-body">
+        <form autocomplete="off" method="post" action="">
+          <div>
+            <a class="btn bg-warning" href="./?hotspot=user-profiles&session=<?= $session; ?>"> <i class="fa fa-close btn-mrg"></i> <?= $_close ?></a>
+            <button type="submit" name="save" class="btn bg-primary btn-mrg"><i class="fa fa-save btn-mrg"></i> <?= $_save ?></button>
+          </div>
+          <table class="table">
+            <tr>
+              <td class="align-middle"><?= $_name ?></td>
+              <td><input class="form-control" type="text" onchange="remSpace();" autocomplete="off" name="name" value="" required="1" autofocus></td>
+            </tr>
+            <tr>
+              <td class="align-middle">Local Address</td>
+              <td><input class="form-control" type="text" size="4" autocomplete="off" name="localaddress" required="1"></td>
+            </tr>
+            <tr>
+              <td class="align-middle">Remote Address</td>
+              <td><input class="form-control" type="text" size="4" autocomplete="off" name="remoteaddress" required="1"></td>
+            </tr>
+            <tr>
+              <td class="align-middle">Bridge</td>
+              <td><input class="form-control" type="text" size="4" autocomplete="off" name="bridge" required="0"></td>
+            </tr>
+            <tr>
+              <td class="align-middle">Rate Limit</td>
+              <td><input class="form-control" type="text" size="4" autocomplete="off" name="retelimit" placeholder="rx/tx" required="1"></td>
+            </tr>
+            <tr>
+              <td class="align-middle">Only One</td>
+              <td>
+                <input type="radio" id="" name="onlyone" value="default">
+                <label for="">Default</label><br>
+                <input type="radio" id="no" name="onlyone" value="no">
+                <label for="no">No</label><br>
+                <input type="radio" id="yes" name="onlyone" value="yes">
+                <label for="yes">Yes</label><br>
+              </td>
+            </tr>
+          </table>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="col-4">
+    <div class="card">
+      <div class="card-header">
+        <h3><i class="fa fa-book"></i> <?= $_readme ?></h3>
+      </div>
+      <div class="card-body">
+        <table class="table">
+          <tr>
+            <td colspan="2">
+              <p style="padding:0px 5px;">
+                <?= $_details_user_profile ?>
+              </p>
+              <p style="padding:0px 5px;">
+                <?= $_format_validity ?>
+              </p>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+  function remSpace() {
+    var upName = document.getElementsByName("name")[0];
+    var newUpName = upName.value.replace(/\s/g, "-");
+    //alert("<?php if ($currency == in_array($currency, $cekindo['indo'])) {
+                echo "Nama Profile tidak boleh berisi spasi";
+              } else {
+                echo "Profile name can't containing white space!";
+              } ?>");
+    upName.value = newUpName;
+    upName.focus();
+  }
+</script>
---
+++ ppp/userprofile.php
@@ -0,0 +1,118 @@
+<?php
+/*
+ *  Copyright (C) 2018 Laksamadi Guko.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// hide all error
+error_reporting(0);
+if (!isset($_SESSION["mikhmon"])) {
+	echo '
+<html>
+<head><title>403 Forbidden</title></head>
+<body bgcolor="white">
+<center><h1>403 Forbidden</h1></center>
+<hr><center>nginx/1.14.0</center>
+</body>
+</html>
+';
+} else {
+
+
+	// get user profile
+	$getprofile = $API->comm("/ppp/profile/print");
+	$TotalReg = count($getprofile);
+	// count user profile
+	$countprofile = $API->comm("/ppp/profile/print", array(
+		"count-only" => "",
+	));
+}
+?>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header align-middle">
+                <h3><i class=" fa fa-pie-chart"></i> PPP Profile
+                    &nbsp; | &nbsp; <a href="./?user-profile=add&session=<?= $session; ?>" title="Add User"><i
+                            class="fa fa-user-plus"></i> Add</a>
+                </h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <div class="overflow box-bordered" style="max-height: 75vh">
+                    <table id="tFilter" class="table table-bordered table-hover text-nowrap">
+                        <thead>
+                            <tr>
+                                <th style="min-width:50px;" class="text-center">
+                                    <?php
+									if ($countprofile < 2) {
+										echo "$countprofile item  ";
+									} elseif ($countprofile > 1) {
+										echo "$countprofile items   ";
+									}
+									?></th>
+                                <th class="align-middle"><?= $_name ?></th>
+                                <th class="align-middle">Local<br>Address</th>
+                                <th class="align-middle">Remote<br>Address</th>
+                                <th class="align-middle">Bridge</th>
+                                <th class="align-middle">Rate<br>Limit</th>
+                                <th class="align-middle">Only<br>One</th>
+                                <th class="align-middle">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+
+							for ($i = 0; $i < $TotalReg; $i++) {
+
+								$profiledetalis = $getprofile[$i];
+								$pid = $profiledetalis['.id'];
+								$pname = $profiledetalis['name'];
+								$local_address = $profiledetalis['local-address'];
+								$remote_address = $profiledetalis['remote-address'];
+								$bridge = $profiledetalis['bridge'];
+								$rate_limit = $profiledetalis['rate-limit'];
+								$only_one = $profiledetalis['only-one'];
+								echo "<tr>";
+							?>
+                            <td style='text-align:center;'><i class='fa fa-minus-square text-danger pointer'
+                                    onclick="if(confirm('Are you sure to delete profile (<?= $pname; ?>)?')){loadpage('./?remove-user-profile=<?= $pid; ?>&pname=<?= $pname ?>&session=<?= $session; ?>')}else{}"
+                                    title='Remove <?= $pname; ?>'></i>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                <?php
+								echo "<a title='Open User by profile " . $pname . "'  href='./?hotspot=users&profile=" . $pname . "&session=" . $session . "'><i class='fa fa-users'></i></a></td>";
+								echo "<td><a title='Open User Profile " . $pname . "' href='./?user-profile=" . $pid . "&session=" . $session . "'><i class='fa fa-edit'></i> <i class='fa fa-ci fa-circle " . $moncolor . "'></i> $pname</a></td>";
+								//$profiledetalis = $ARRAY[$i];echo "<td>" . $profiledetalis['name'];echo "</td>";
+								echo "<td>" . $local_address;
+								echo "</td>";
+								echo "<td>" . $remote_address;
+								echo "</td>";
+								echo "<td>" . $bridge;
+								echo "</td>";
+								echo "<td>" . $rate_limit;
+								echo "</td>";
+								echo "<td>" . $only_one;
+								echo "</td>";
+								echo "<td>" . $only_one;
+								echo "</td>";
+								echo "</tr>";
+							}
+								?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
