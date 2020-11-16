26b141873991e038d04d8d03fcebdf0daab48e09
afiffarishudaifi <afiffaris5@gmail.com>
2020-11-16 18:47:55 +0700

    link index.php

--- index.php
+++ index.php
@@ -39,21 +39,21 @@ if (!isset($_SESSION["mikhmon"])) {
 
   $_SESSION["connect"] = "";
 
-  // time zone
+// time zone
   date_default_timezone_set($_SESSION['timezone']);
 
-  // lang
+// lang
   include('./include/lang.php');
-  include('./lang/' . $langid . '.php');
+  include('./lang/'.$langid.'.php');
 
-  // quick bt
+// quick bt
   include('./include/quickbt.php');
 
-  // load config
+// load config
   include('./include/config.php');
   include('./include/readcfg.php');
 
-  // theme  
+// theme  
   include('./include/theme.php');
   include('./settings/settheme.php');
   if ($_SESSION['theme'] == "") {
@@ -64,7 +64,7 @@ if (!isset($_SESSION["mikhmon"])) {
     $themecolor = $_SESSION['themecolor'];
   }
 
-  // routeros api
+// routeros api
   include_once('./lib/routeros_api.class.php');
   include_once('./lib/formatbytesbites.php');
   $API = new RouterosAPI();
@@ -73,9 +73,9 @@ if (!isset($_SESSION["mikhmon"])) {
 
   $getidentity = $API->comm("/system/identity/print");
   $identity = $getidentity[0]['name'];
+  
 
-
-  // get variable
+// get variable
   $hotspot = $_GET['hotspot'];
   $hotspotuser = $_GET['hotspot-user'];
   $userbyname = $_GET['hotspot-user'];
@@ -117,9 +117,9 @@ if (!isset($_SESSION["mikhmon"])) {
   $minterface = $_GET['interface'];
 
 
-  $pagehotspot = array('users', 'hosts', 'ipbinding', 'cookies', 'log', 'dhcp-leases');
-  $pageppp = array('secrets', 'profiles', 'active',);
-  $pagereport = array('userlog', 'selling');
+  $pagehotspot = array('users','hosts','ipbinding','cookies','log','dhcp-leases');
+  $pageppp = array('secrets','profiles','active',);
+  $pagereport = array('userlog','selling');
 
   include_once('./include/headhtml.php');
 
@@ -134,7 +134,7 @@ if (!isset($_SESSION["mikhmon"])) {
 </script>';
 
 
-  // logout
+// logout
   if ($hotspot == "logout") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Logout...</b>";
 
@@ -142,48 +142,49 @@ if (!isset($_SESSION["mikhmon"])) {
     echo "<script>sessionStorage.clear();</script>";
     echo "<script>window.location='./admin.php?id=login'</script>";
   }
-  // redirect to home
-  elseif (substr(explode("=", $url)[0], -9) == "/?session") {
+// redirect to home
+  elseif (substr(explode("=", $url)[0],-9) == "/?session") {
 
     include_once('./dashboard/home.php');
     $_SESSION['ubn'] = "";
   }
 
-  // redirect to home
+// redirect to home
   elseif ($hotspot == "dashboard") {
     include_once('./dashboard/home.php');
     $_SESSION['ubn'] = "";
+
   }
 
-  // hotspot log
+// hotspot log
   elseif ($hotspot == "log") {
     include_once('./hotspot/log.php');
   }
 
-  // hotspot log
+// hotspot log
   elseif ($report == "userlog") {
     include_once('./report/userlog.php');
   }
 
-  // about
+// about
   elseif ($hotspot == "about") {
     include_once('./include/about.php');
   }
 
-  // bad request
+// bad request
   elseif (substr($url, -1) == "=") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Bad request! redirect to Home......</b>";
 
     echo "<script>window.location='./'</script>";
   }
 
-  // hotspot add users
+// hotspot add users
   elseif ($hotspot == "add-user") {
     $_SESSION['hua'] = "";
     include_once('./hotspot/adduser.php');
   }
 
-  // hotspot users
+// hotspot users
   elseif ($hotspot == "users" && $prof == "all") {
     $_SESSION['ubp'] = "";
     $_SESSION['hua'] = "";
@@ -192,7 +193,7 @@ if (!isset($_SESSION["mikhmon"])) {
     include_once('./hotspot/users.php');
   }
 
-  // hotspot users filter by profile
+// hotspot users filter by profile
   elseif ($hotspot == "users" && $prof != "") {
     $_SESSION['ubp'] = $prof;
     $_SESSION['hua'] = "";
@@ -201,7 +202,7 @@ if (!isset($_SESSION["mikhmon"])) {
     include_once('./hotspot/users.php');
   }
 
-  // hotspot users filter by comment
+// hotspot users filter by comment
   elseif ($hotspot == "users" && $comm != "") {
     $_SESSION['ubc'] = $comm;
     $_SESSION['hua'] = "";
@@ -210,7 +211,7 @@ if (!isset($_SESSION["mikhmon"])) {
     include_once('./hotspot/users.php');
   }
 
-  // hotspot by profile
+// hotspot by profile
   elseif ($hotspot == "users-by-profile") {
     $_SESSION['ubp'] = "";
     $_SESSION['hua'] = "";
@@ -218,34 +219,34 @@ if (!isset($_SESSION["mikhmon"])) {
     $_SESSION['vcr'] = "active";
     include_once('./hotspot/userbyprofile.php');
   }
-  // export hotspot users
+// export hotspot users
   elseif ($hotspot == "export-users") {
     include_once('./hotspot/exportusers.php');
   }
 
-  // quick print
+// quick print
   elseif ($hotspot == "quick-print") {
     include_once('./hotspot/quickprint.php');
   }
 
-  // quick print
-  elseif ($hotspot == "list-quick-print") {
-    include_once('./hotspot/listquickprint.php');
-  }
+// quick print
+elseif ($hotspot == "list-quick-print") {
+  include_once('./hotspot/listquickprint.php');
+}  
 
-  // add hotspot user
+// add hotspot user
   elseif ($hotspotuser == "add") {
     include_once('./hotspot/adduser.php');
     echo $disable_sci;
   }
 
-  // add hotspot user
+// add hotspot user
   elseif ($hotspotuser == "generate") {
     include_once('./hotspot/generateuser.php');
     echo $disable_sci;
   }
 
-  // hotspot users filter by name
+// hotspot users filter by name
   elseif (substr($hotspotuser, 0, 1) == "*") {
     $_SESSION['ubn'] = $hotspotuser;
     $_SESSION['hua'] = "";
@@ -255,59 +256,59 @@ if (!isset($_SESSION["mikhmon"])) {
     include_once('./hotspot/userbyname.php');
   }
 
-  // remove hotspot user
+// remove hotspot user
   elseif ($removehotspotuser != "" || $removehotspotusers != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
     include_once('./process/removehotspotuser.php');
   }
 
-  // remove hotspot user by comment
+// remove hotspot user by comment
   elseif ($removehotspotuserbycomment != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
     include_once('./process/removehotspotuserbycomment.php');
   }
 
-  // remove expired hotspot user
-  elseif ($removeexpiredhotspotuser != "") {
-    echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
+// remove expired hotspot user
+elseif ($removeexpiredhotspotuser != "") {
+  echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
-    include_once('./process/removeexpiredhotspotuser.php');
-  }
+  include_once('./process/removeexpiredhotspotuser.php');
+}  
 
-  // reset hotspot user
+// reset hotspot user
   elseif ($resethotspotuser != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
     include_once('./process/resethotspotuser.php');
   }
 
-  // enable hotspot user
+// enable hotspot user
   elseif ($enablehotspotuser != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
     include_once('./process/enablehotspotuser.php');
   }
 
-  // disable hotspot user
+// disable hotspot user
   elseif ($disablehotspotuser != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
     include_once('./process/disablehotspotuser.php');
   }
 
-  // user profile
+// user profile
   elseif ($hotspot == "user-profiles") {
     include_once('./hotspot/userprofile.php');
   }
 
-  // add  user profile
+// add  user profile
   elseif ($userprofile == "add") {
     include_once('./hotspot/adduserprofile.php');
   }
 
-  // User profile by name
+// User profile by name
   elseif (substr($userprofile, 0, 1) == "*") {
     include_once('./hotspot/userprofilebyname.php');
   } elseif ($userprofile != "") {
@@ -315,14 +316,14 @@ if (!isset($_SESSION["mikhmon"])) {
   }
 
 
-  // remove user profile
+// remove user profile
   elseif ($removeuserprofile != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
     include_once('./process/removeuserprofile.php');
   }
 
-  // hotspot active
+// hotspot active
   elseif ($hotspot == "active") {
     $_SESSION['ubp'] = "";
     $_SESSION['hua'] = "hotspotactive";
@@ -330,54 +331,54 @@ if (!isset($_SESSION["mikhmon"])) {
     include_once('./hotspot/hotspotactive.php');
   }
 
-  // dhcp leases
+// dhcp leases
   elseif ($hotspot == "dhcp-leases") {
     include_once('./dhcp/dhcpleases.php');
   }
 
-  // traffic monitor
+// traffic monitor
   elseif ($minterface == "traffic-monitor") {
-    include_once('./traffic/trafficmonitor.php');
-  }
+  include_once('./traffic/trafficmonitor.php');
+}
 
-  // hotspot hosts
+// hotspot hosts
   elseif ($hotspot == "hosts" || $hotspot == "hostp" || $hotspot == "hosta") {
     include_once('./hotspot/hosts.php');
   }
 
-  // hotspot bindings
+// hotspot bindings
   elseif ($hotspot == "binding") {
     include_once('./hotspot/binding.php');
   }
 
-  // template editor
+// template editor
   elseif ($hotspot == "template-editor") {
     include_once('./settings/vouchereditor.php');
   }
 
-  // upload logo
+// upload logo
   elseif ($hotspot == "uplogo") {
     include_once('./settings/uplogo.php');
   }
 
-  // hotspot Cookies
+// hotspot Cookies
   elseif ($hotspot == "cookies") {
     include_once('./hotspot/cookies.php');
   }
 
-  // remove hotspot Cookies
+// remove hotspot Cookies
   elseif ($removecookie != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
     include_once('./process/removecookie.php');
   }
 
-  // hotspot Ip Bindings
+// hotspot Ip Bindings
   elseif ($hotspot == "ipbinding") {
     include_once('./hotspot/ipbinding.php');
   }
 
-  // remove enable disable ipbinding
+// remove enable disable ipbinding
   elseif ($removeipbinding != "" || $enableipbinding != "" || $disableipbinding != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
@@ -385,14 +386,14 @@ if (!isset($_SESSION["mikhmon"])) {
   }
 
 
-  // remove user active
+// remove user active
   elseif ($removeuseractive != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
     include_once('./process/removeuseractive.php');
   }
 
-  // remove host
+// remove host
   elseif ($removehost != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
@@ -400,51 +401,51 @@ if (!isset($_SESSION["mikhmon"])) {
   }
 
 
-  // makebinding
+// makebinding
   elseif ($macbinding != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
     include_once('./process/makebinding.php');
   }
 
-  // selling
+// selling
   elseif ($report == "selling") {
     include_once('./report/selling.php');
   }
 
-  // selling
-  elseif ($report == "resume-report") {
-    include_once('./report/resumereport.php');
-  }
+// selling
+elseif ($report == "resume-report") {
+  include_once('./report/resumereport.php');
+}
 
-  // selling
-  elseif ($report == "export") {
-    include_once('./report/export.php');
-  }
+// selling
+elseif ($report == "export") {
+  include_once('./report/export.php');
+}
 
-  // selling
+// selling
   elseif ($removereport != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
     include_once('./process/removereport.php');
   }
 
-  // ppp secret
+// ppp secret
   elseif ($ppp == "secrets") {
     include_once('./ppp/pppsecrets.php');
   }
 
-  // ppp addsecret
+// ppp addsecret
   elseif ($ppp == "addsecret") {
     include_once('./ppp/addsecret.php');
   }
 
-  // ppp secretbyname
+// ppp secretbyname
   elseif ($secretbyname != "") {
     include_once('./ppp/secretbyname.php');
   }
 
-  // remove enable disable secret
+// remove enable disable secret
   elseif ($removesecr != "" || $enablesecr != "" || $disablesecr != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
@@ -452,68 +453,68 @@ if (!isset($_SESSION["mikhmon"])) {
   }
 
 
-  // ppp profile
-  elseif ($ppp == "profiles") {
-    include_once('./ppp/pppprofile.php');
-  }
-
-  // add ppp profile
-  elseif ($ppp == "add-profile") {
-    include_once('./ppp/addpppprofile.php');
-  }
-
-  // ppp profile by name
-  elseif ($ppp == "edit-profile") {
-    include_once('./ppp/profilebyname.php');
-  }
-  // elseif (substr($ppp, 0, 1) == "*") {
-  //   include_once('./ppp/profilebyname.php');
-  // } elseif ($ppp != "") {
-  //   include_once('./ppp/profilebyname.php');
-  // }
-  // remove enable disable profile
-  elseif ($removepprofile != "") {
-    echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
-
-    include_once('./process/removepprofile.php');
-  }
-
-  // ppp active connection
+// ppp active connection
   elseif ($ppp == "active") {
     include_once('./ppp/pppactive.php');
   }
 
-  // remove ppp active connection
+// remove ppp active connection
   elseif ($removepactive != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
     include_once('./process/removepactive.php');
   }
 
-  // sys scheduler
+// ppp profile
+  elseif ($ppp == "profiles") {
+    include_once('./ppp/pppprofile.php');
+  }
+
+// add ppp profile
+  elseif ($ppp == "add-profile") {
+    include_once('./ppp/addpppprofile.php');
+  }
+
+// ppp profile by name
+// elseif ($ppp == "edit-profile") {
+//   include_once('./ppp/profilebyname.php');
+// }
+  elseif (substr($ppp, 0, 1) == "*") {
+    include_once('./ppp/profilebyname.php');
+  } elseif ($ppp != "") {
+    include_once('./ppp/profilebyname.php');
+  }
+// remove enable disable profile
+  elseif ($removepprofile != "") {
+    echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
+
+    include_once('./process/removepprofile.php');
+  }
+
+// sys scheduler
   elseif ($sys == "scheduler") {
     include_once('./system/scheduler.php');
   }
-  // remove enable disable scheduler
+// remove enable disable scheduler
   elseif ($removesch != "" || $enablesch != "" || $disablesch != "") {
     echo "<b class='cl-w'><i class='fa fa-circle-o-notch fa-spin' style='font-size:24px'></i> Processing...</b>";
 
     include_once('./process/pscheduler.php');
   }
 
-?>
+  ?>
 
-  </div>
-  </div>
-  </div>
-  <script src="./js/highcharts/highcharts.js"></script>
-  <script src="./js/highcharts/themes/hc.<?= $theme; ?>.js"></script>
-  <script src="./js/mikhmon-ui.<?= $theme; ?>.min.js"></script>
-  <script src="./js/mikhmon.js?t=<?= str_replace(" ", "_", date("Y-m-d H:i:s")); ?>"></script>
+</div>
+</div>
+</div>
+<script src="./js/highcharts/highcharts.js"></script>
+<script src="./js/highcharts/themes/hc.<?= $theme; ?>.js"></script>
+<script src="./js/mikhmon-ui.<?= $theme; ?>.min.js"></script>
+<script src="./js/mikhmon.js?t=<?= str_replace(" ","_",date("Y-m-d H:i:s")); ?>"></script>
 
 <?php
-  if ($hotspot == "dashboard" || substr(end(explode("/", $url)), 0, 8) == "?session") {
-    echo '<script>
+if ($hotspot == "dashboard" || substr(end(explode("/", $url)), 0, 8) == "?session") {
+  echo '<script>
     $("#r_3").load("./dashboard/aload.php?session=' . $session . '&load=logs #r_3");  
     var interval1 = "' . ($areload * 1000) . '";
     var dashboard = setInterval(function() {
@@ -525,50 +526,50 @@ if (!isset($_SESSION["mikhmon"])) {
   }, interval1);
 
 ';
-    if ($livereport == "enable" || $livereport == "") {
-      if ($_SESSION[$session . 'sdate'] != $_SESSION[$session . 'idhr']) {
-        $_SESSION[$session . 'totalHr'] = "0";
-        echo '$("#r_4").load("./report/livereport.php?session=' . $session . ' #r_4");';
-      } else if ($_SESSION[$session . 'sdate'] == $_SESSION[$session . 'idhr']) {
-      } else {
-        echo '$("#r_4").load("./report/livereport.php?session=' . $session . ' #r_4");';
-      }
-      echo  '
+if ($livereport == "enable" || $livereport == "") {
+  if($_SESSION[$session.'sdate'] != $_SESSION[$session.'idhr']){
+    $_SESSION[$session.'totalHr'] = "0";
+    echo '$("#r_4").load("./report/livereport.php?session=' . $session . ' #r_4");';
+    }else if ($_SESSION[$session.'sdate'] == $_SESSION[$session.'idhr']){  
+    }else{
+      echo '$("#r_4").load("./report/livereport.php?session=' . $session . ' #r_4");';
+    }
+  echo  '
     var interval2 = "65432";
     var livereport = setInterval(function() {
     $("#r_4").load("./report/livereport.php?session=' . $session . ' #r_4"); 
   }, interval2);
- ';
-    }
-    echo ' 
+ ';}
+  echo ' 
   function cancelPage(){
     window.stop();
     clearInterval(dashboard);';
     if ($livereport == "enable" || $livereport == "") {
-      echo '
+    echo '
     clearInterval(livereport);';
     }
-    echo '
+  echo '
     }
 </script>';
-  } elseif ($hotspot == "active" && $serveractive != "") {
-    echo '<script>
+
+} elseif ($hotspot == "active" && $serveractive != "") {
+  echo '<script>
   $(document).ready(function(){
     var interval = "' . ($areload * 1000) . '";
     setInterval(function() {
     $("#reloadHotspotActive").load("./hotspot/hotspotactive.php?server=' . $serveractive . '&session=' . $session . '"); }, interval);})
 </script>
 ';
-  } elseif ($hotspot == "active" && $serveractive == "") {
-    echo '<script>
+} elseif ($hotspot == "active" && $serveractive == "") {
+  echo '<script>
   $(document).ready(function(){
     var interval = "' . ($areload * 1000) . '";
     setInterval(function() {
     $("#reloadHotspotActive").load("./hotspot/hotspotactive.php?session=' . $session . '"); }, interval);})
 </script>
 ';
-  } elseif ($userprofile == "add" || substr($userprofile, 0, 1) == "*" || $userprofile != "") {
-    echo "<script>
+} elseif ($userprofile == "add" || substr($userprofile, 0, 1) == "*" || $userprofile != "") {
+  echo "<script>
   //enable disable input on ready
 $(document).ready(function(){
     var exp = document.getElementById('expmode').value;
@@ -588,8 +589,9 @@ $(document).ready(function(){
     }
 });
 </script>";
-  } elseif (in_array($hotspot, $pagehotspot) || in_array($ppp, $pageppp) || in_array($report, $pagereport) || $sys == "scheduler") {
-    echo '
+
+} elseif (in_array($hotspot, $pagehotspot) || in_array($ppp, $pageppp) || in_array($report, $pagereport) || $sys == "scheduler") {
+echo '
 <script>
 $(document).ready(function(){
   makeAllSortable();
@@ -603,8 +605,8 @@ $(document).ready(function(){
 
 </script>
 ';
-  } elseif ($ppp == "add" || substr($ppp, 0, 1) == "*" || $ppp != "") {
-    echo "<script>
+} elseif ($ppp == "add" || substr($ppp, 0, 1) == "*" || $ppp != "") {
+  echo "<script>
   //enable disable input on ready
 $(document).ready(function(){
     var exp = document.getElementById('expmode').value;
@@ -624,9 +626,10 @@ $(document).ready(function(){
     }
 });
 </script>";
-  }
+
+} 
 }
 ?>
 </body>
-
 </html>
+
